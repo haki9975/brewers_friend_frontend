97b774523ff0d72c38b0fc657ef3d24cdea509b8
haki9975 <elias.robert.hakim@gmail.com>
2021-05-25 12:10:27 -0400

    Initialize first fetch to /beers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Woooooooooooord!")
+fetch("http://localhost:3000/beers")
+    .then(response => response.json())
+        .then(beers => console.log(beers))
+            .catch(error => console.warn(error))
+
