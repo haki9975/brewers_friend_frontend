29ff68563e5434525d5e5a0e6f63fb10b826d31f
haki9975 <elias.robert.hakim@gmail.com>
2021-05-25 14:46:05 -0400

    Add DOM Content Loaded listener, building out form.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-console.log("hello!")
+document.addEventListener("DOMContentLoaded", () => {
+    getBeers();
+    createForm();
+})
+
 
 function getBeers(){
+   const beerList = document.getElementById("beerList")
     fetch("http://localhost:3000/beers")
         .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => {
+                data.forEach(beer => {
+                    beerList.innerHTML += `<li>${beer.name}</li>`
+                })
+            })
                 .catch(error => console.warn(error))
 }
 
+function createForm(){
+    const scaleContainer = document.getElementById("scale-container")
+    const form = document.createElement('form')
+        form.innerHTML = `<input placeholder='name' type=text /><input type='submit'/>`    
+}
