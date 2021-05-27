84029da6789c46e73ac42a622ea526ba5b523801
haki9975 <elias.robert.hakim@gmail.com>
2021-05-27 12:54:38 -0400

    Move beer and ingredient classes to new source files

---
+++ src/beer.js
@@ -0,0 +1,15 @@
+
+class Beer {
+    constructor(name, description, abv, ibu, volume, boil_volume, mash_instruct, fermentation_instruct, food_pairing, tips) {
+        this.name = name
+        this.description = description
+        this.abv = abv
+        this.ibu = ibu
+        this.volume = volume
+        this.boil_volume = boil_volume
+        this.mash_instruct = mash_instruct
+        this.fermentation_instruct = fermentation_instruct
+        this.food_pairing = food_pairing
+        this.tips = tips
+    }    
+}
---
+++ src/ingredient.js
@@ -0,0 +1,10 @@
+
+
+class Ingredient {
+    constructor(name, category, amount, unit){
+        this.name = name
+        this.category = category
+        this.amount = amount
+        this.unit = unit
+    }
+}
