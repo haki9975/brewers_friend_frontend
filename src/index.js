f54cd31e463c43d65cfc9ba4e99d0d95efe61551
haki9975 <elias.robert.hakim@gmail.com>
2021-05-27 21:49:58 -0400

    Write JS functions for model activation/deactivation

--- src/index.js
+++ src/index.js
@@ -83,6 +83,25 @@ openModalButtons.forEach(button => {
     })
 })   
 
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal =  button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
  //function handleScale(e){
     //     e.preventDefault()
     //     const scaleInput = e.target.children[0]
