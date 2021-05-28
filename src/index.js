03b07a8ef8e11a5e4f89218e02c33e2939065a47
haki9975 <elias.robert.hakim@gmail.com>
2021-05-27 22:26:19 -0400

    Clean up old notes with abandoned functions

--- src/index.js
+++ src/index.js
@@ -10,20 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
       logTarget()
 })
 
-
-// function getBeers(){
-//    const beerList = document.getElementById("beerList")
-//     fetch("http://localhost:3000/beers")
-//         .then(response => response.json())
-//             .then(data => {
-//                 data.forEach(beer => {
-//                     beerList.innerHTML += `<li>${beer.name}</li>`
-//                 })
-//             })
-//                 .catch(error => console.warn(error))
-// } //shows list item of beer names. You want to add a listener for these and expand them when they click 
-
-
 function createBeerForm(){
     const beerContainer = document.getElementById("modal-body")
     const form = document.createElement("form")
@@ -105,27 +91,3 @@ function closeModal(modal) {
     overlay.classList.remove('active')
 }
 
- //function handleScale(e){
-    //     e.preventDefault()
-    //     const scaleInput = e.target.children[0]
-    
-    //     fetch("http://localhost:3000/beer/:id", {
-        //         method: "POST",
-        //         headers: {
-            //             "Content-Type": "application/json",
-            //             Accept: "application/json"
-            //         },
-            //         body: JSON.stringify({
-                //             name: scaleInput.value
-                //         })
-                //     })
- //}
-
-
-//function createScaleForm(){
-    //     const scaleContainer = document.getElementById("scale-container")
-    //     const form = document.createElement('form')
-    //         form.innerHTML = `<input placeholder='Scale your recipe!' type=text /><br><input type='submit'/>`
-    //         scaleContainer.append(form)
-    //         form.addEventListener("submit", handleScale)  
- //} //create scaling form...  abandon this for now
