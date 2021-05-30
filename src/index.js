b8322d5569d12ac124df5ab083819ebaa5a95ed5
haki9975 <elias.robert.hakim@gmail.com>
2021-05-29 21:08:38 -0400

    Move delete event listener, remove event target element upon deletion

--- src/index.js
+++ src/index.js
@@ -61,15 +61,17 @@ function renderBeer(){
         recipe.innerHTML = `<b>Name:</b> ${beer.name} <br> <b>Description:</b> ${beer.description}<br> <b>ABV:</b> ${beer.abv}<br> <b>IBU:</b> ${beer.ibu}<br> <b>Total Volume:</b> ${beer.volume} Liters<br> <b>Boil Volume:</b> ${beer.boil_volume} Liters<br> <b>Mash Instructions:</b> ${beer.mash_instruct}<br> <b>Fermentation Instructions:</b> ${beer.fermentation_instruct}<br> <b>Suggested Food Pairings:</b> ${beer.food_pairing}<br> <b>Brewer's Tips:</b> ${beer.tips}<br><br>`
         ing.forEach(i => recipe.innerHTML += `<b>Ingredient Number: ${ing.indexOf(i) + 1}</b><br><b>Name:</b> ${i.name}<br><b>Category:</b> ${i.category}<br><b>Amount:</b> ${i.amount} ${i.unit}<br><br>`)
         _e.append(recipe)
+        _delete.addEventListener("click", function(){
+            _e.remove()
+            debugger
+            _url.deleteBeer(beer)
+        })
+        
         _e.addEventListener("click", function() {
             _e.removeChild(recipe)
             console.log("hello")
         })//closes rendered recipe.
         recipe.append(_delete)
-        _delete.onclick = function(){
-            _url.deleteBeer(beer)
-            p.remove()
-        }
     })
 
 }// eventlistener for beerList container. Renders Recipes and removes recipe rendering.
