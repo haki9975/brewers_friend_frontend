9538f94a5dde6af2aca3c7282c720fbb3642d8fe
haki9975 <elias.robert.hakim@gmail.com>
2021-08-18 23:16:54 -0700

    Remove notes from beerApi.js

--- src/beerApi.js
+++ src/beerApi.js
@@ -15,39 +15,6 @@ class BeerApi {
       .catch((error) => console.warn(error));
   }
 
-  //   addBeers(nameInput, desc, abv, ibu, vol, bvol, mash, ferm, pairings, tips) {
-  //     fetch(this.beerUrl, {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Accept: "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         name: nameInput.value,
-  //         description: desc.value,
-  //         abv: abv.value,
-  //         ibu: ibu.value,
-  //         volume: vol.value,
-  //         boil_volume: bvol.value,
-  //         mash_instruct: mash.value,
-  //         fermentation_instruct: ferm.value,
-  //         food_pairings: pairings.value,
-  //         tips: tips.value,
-  //       }),
-  //     })
-  //       .then((resp) => resp.json())
-  //       .then((data) => {
-  //         // console.log(data)
-  //         // if (data.status === 201){
-  //         //    // this.addBeers(data.beer)
-  //         // } else {
-  //         //     alert(data.errors)
-  //         // }
-  //         // nameInput.value = ""
-  //       })
-  //       .catch((error) => alert(error.message));
-  //   }
-
   addBeers(beer) {
     fetch(this.beerUrl, {
       method: "POST",
